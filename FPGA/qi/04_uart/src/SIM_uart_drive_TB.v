c790e52c9896cbc878e8d6b0b91efbe35daa872a
fzwwj95 <fzwwj95@163.com>
2025-05-22 00:11:25 +0800

    add TB file for uart

---
+++ FPGA/qi/04_uart/src/SIM_uart_drive_TB.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/05/21 23:27:34
+// Design Name: 
+// Module Name: SIM_uart_drive_TB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SIM_uart_drive_TB(
+
+    );
+
+reg    clk,rst    ;
+
+initial begin
+    rst = 1       ;
+    #100          ;
+    rst = 0       ;
+    
+
+end
+
+always @() begin
+    
+end
+endmodule
